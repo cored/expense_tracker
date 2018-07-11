b86d380ceab2e416da6eb39af3d846c194a05ba3
Rafael George <george.rafael@gmail.com>
2018-07-11 09:16:23 -0400

    POST a new expense
    Add acceptance to test drive the creation of a new expense

---
+++ app/api.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'sinatra/base'
+require 'json'
+
+module ExpenseTracker
+  class API < Sinatra::Base
+    post '/expenses' do
+      JSON.generate('expense_id' => 42)
+    end
+  end
+end
